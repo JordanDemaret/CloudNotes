13980585517c1abbb583d0f733482a57c08ec338
JordanDemaret <jordandemaret12@gmail.com>
2026-09-22 10:49:17 +0200

    Ajoutez des fichiers projet.

---
+++ CloudNotes/Controllers/NoteController.cs
@@ -0,0 +1,53 @@
+﻿using CloudNotes.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CloudNotes.Controllers
+{
+    [ApiController]
+    [Route("note")]
+    public class NoteController : ControllerBase
+    {
+
+        private static IList<Note> _notes = new List<Note>()
+        {
+            new Note(){Description = "note sur les fondamantal de c#"} ,
+            new Note(){Description= "note sur les POO de c#"},
+            new Note(){Description= "note Angular"},
+            new Note(){Description= "note Azure"}
+        };
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok(_notes);
+        }
+
+        [HttpPost]
+        public IActionResult Post(string description)
+        {
+            _notes.Add(new Note() { Description = description });
+            return Ok();
+        }
+
+        [HttpPut]
+        public IActionResult Put(Guid id, string description)
+        {
+            Note? note = _notes.SingleOrDefault(n => n.Id == id);
+            if (note is null)
+                return BadRequest();
+            note.Description = description;
+            return Ok();
+        }
+
+        [HttpDelete]
+        public IActionResult Delete(Guid id)
+        {
+            Note? note = _notes.SingleOrDefault(n => n.Id == id);
+            if (note is null)
+                return BadRequest();
+            _notes.Remove(note);
+            return Ok();
+        }
+
+    }
+}
---
+++ CloudNotes/Models/Note.cs
@@ -0,0 +1,10 @@
+﻿namespace CloudNotes.Models
+{
+    public record Note
+    {
+        public Guid Id { get; } = Guid.NewGuid();
+
+        public string Description { get; set; } = string.Empty;
+    }
+}
+        
